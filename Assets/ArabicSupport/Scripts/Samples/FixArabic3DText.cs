a22d0f81bfd2b83fedd4d507f9c39f2066b0f301
FaizanAhmed <faizanahmed21@hotmail.com>
2026-07-29 00:21:58 +0500

    Arabic text support

---
+++ Assets/ArabicSupport/Scripts/Samples/FixArabic3DText.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using ArabicSupport;
+
+public class FixArabic3DText : MonoBehaviour {
+
+    public bool showTashkeel = true;
+    public bool useHinduNumbers = true;
+
+    // Use this for initialization
+    void Start () {
+        Text text = gameObject.GetComponent<Text>();
+
+        string fixedText = ArabicFixer.Fix(text.text, showTashkeel, useHinduNumbers);
+
+        gameObject.GetComponent<Text>().text = fixedText;
+
+		Debug.Log(fixedText);
+    }
+
+}
